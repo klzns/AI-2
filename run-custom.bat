df8236847d8c0e1109a2280b9b0166a571b536aa
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-11 23:25:32 -0300

    fix bats

--- run-custom.bat
+++ run-custom.bat
@@ -1,15 +1,15 @@
 cd src
 
 echo "Skipping random items, copying from src/items.txt"
-cp items.txt .\visualization\app\items.txt
+cp items.txt ./visualization/app/items.txt
 
 echo "Generating facts"
 ruby generator/fact-generator.rb > facts.pl
 
 echo "Generating action log"
-python logic/logic.py > .\visualization\app\action-log.json
+python logic/logic.py > ./visualization/app/action-log.json
 
 echo "Running gulp"
-cd .\visualization
+cd visualization
 gulp
 gulp watch
--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 cd src
 
 echo "Generating random items"
-ruby generator/item-generator.rb > .\visualization\app\items.txt
+ruby generator/item-generator.rb > ./visualization/app/items.txt
 
 echo "Generating facts"
 ruby generator/fact-generator.rb > facts.pl
 
 echo "Generating action log"
-python logic/logic.py > .\visualization\app\action-log.json
+python logic/logic.py > ./visualization/app/action-log.json
 
 echo "Running gulp"
-cd .\visualization
+cd visualization
 gulp
 gulp watch
