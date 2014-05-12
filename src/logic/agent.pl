3f3d6c07eddb260b6eb7a599154bdf2821bac84e
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-11 21:52:07 -0300

    dont walk on trees

--- src/logic/agent.pl
+++ src/logic/agent.pl
@@ -40,5 +40,5 @@ safe(20, 37).
 /* BEST ACTIONS, IN ORDER OF PREFERENCE */
 best_action(pick_rupee, none1, none2) :- on(X, Y), detect_rupee(X, Y), retract(item_R(X, Y)).
 best_action(pick_sword, none1, none2) :- on(X, Y), detect_sword(X, Y), retract(item_M(X, Y)), retract(item_F(X, Y)).
-best_action(pick_heart, none1, none2) :- on(X, Y), detect_heart(X, Y), retract(item_C(X, Y)).
-best_action(walk, X, Y) :- safe(X, Y), not(on(X, Y)), not(visited(X, Y)). /* TODO: BFS */
+best_action(pick_heart, none1, none2) :- on(X, Y), detect_heart(X, Y), energy(E), E < 100, retract(item_C(X, Y)).
+best_action(walk, X, Y) :- safe(X, Y), not(on(X, Y)), not(visited(X, Y)), tile_g(X, Y). /* TODO: BFS */
